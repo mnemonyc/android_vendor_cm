213832577b7269fac5dad0b54c61c192b76c968b
Francesco Colella <mnemonyc@hotmail.com>
2015-11-10 22:18:15 +0100

    Added HAZY_VER incremental

--- configs/version.mk
+++ configs/version.mk
@@ -28,6 +28,7 @@ ifeq ($(ROM_BUILDTIME_LOCAL),y)
     ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d-%H%M%z)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
 else
     ROM_VERSION := $(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)-$(ROM_BUILDTYPE)
+    HAZY_VER := 5.0
 endif
 endif
 
