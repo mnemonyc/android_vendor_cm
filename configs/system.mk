b398e693fa7e8cc9b26a68edd47b588bdd8baff5
Francesco Colella <mnemonyc@hotmail.com>
2015-11-27 22:20:41 +0100

    Revert "Add missmetched file and configurations"
    This reverts commit 98497f5ab9ccd820b6de6531ea20de75fede9d60.

--- configs/system.mk
+++ configs/system.mk
@@ -16,8 +16,7 @@ PRODUCT_COPY_FILES += \
     vendor/hazy/prebuilt/common/addon.d/99-backup.sh:system/addon.d/99-backup.sh \
     vendor/hazy/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/hazy/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
-    vendor/hazy/prebuilt/common/etc/backup.conf:system/etc/backup.conf \
-    vendor/hazy/prebuilt/common/bin/install-recovery.sh:system/bin/install-recovery.sh
+    vendor/hazy/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
 # Security Enhanced Linux
 PRODUCT_PROPERTY_OVERRIDES += \
