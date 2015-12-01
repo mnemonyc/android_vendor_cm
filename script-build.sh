459e0e00d3347a9f6b45030ee9e4e77ef7a7e892
Francesco Colella <mnemonyc@hotmail.com>
2015-12-01 18:30:10 +0100

    Don't clean out dir

--- script-build.sh
+++ script-build.sh
@@ -3,13 +3,14 @@ export PATH=/data/bin:$PATH
 export CCACHE_DIR=/data/ccache/users/
 export USE_CCACHE=1
 
-make clean && make clobber
+# make clean && make clobber
 
 source build/envsetup.sh
 
 croot
 
 chmod -R 0777 vendor
+chmod -R 0777 out
 
 brunch shamu
 
